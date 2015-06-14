39f8e7b5724d5dac97a0bbe3b089ecac9162d8dd
Patrick Plocke <patrick@plocke.de>
2015-06-14 16:20:11 +0200

    iQIcBAABCgAGBQJVfY2bAAoJEKrf9TsovxefAw4P/RwniLDKCTDyNrP35Y4yn1M1  drnWsiZZSelICmmyVOXFsgVfkdaRkZPJapPJpQXMkNFw1J9mRn9+v4/3xl8/FksM  uCPfaso4kVVCPoNrZ08mvMlH40dGB6ih+RXgiMWXzZ7sVIBIcNDgnnbcTSki1jCR  p8pLJBCknGJa7Js1nTiWn3nxvZt5rggCzsQI9J3oclc+4oJAyreLZnXVKAwscx0f  hJQsOh9GUkQkYUmqKklWt+/rpkxhaNWU2O8pS3fnvY8KvVdas40G9M4QMBgG9JHx  c0uvareGu4sod5OrTATQbKNlqJJGFFMlfP5SKL5YmdMHZoltk/cTG1K1IXQQxBAX  lDw124+wveUFpFU8PCOSm4ImN9f5hanSAzFxH5ivTHg6WLP3VFHq17aMDlvfBdn8  RLn9PJOob/drNRW+JpG2zoMWjY18SFwIH42pzR+I9Hb3u04022AV3AtEA7/jLHk+  O3XQn+HpobPS/a1+uFTZHyFQLdU4Ah10AcbdksmX9l+Em4KpycuL1ipVniTTmy6D  KOTes4McRTMpEYRvoXG9KZMLc/eUrfiPXFU6Sl3w8FMMkLYI5YvH/VBbQ7b0zZsl  5Q5V0+v5v8d8YekbJkmqmhMO5HAhUHGv/R3VUFFgn66Iz3VBokzRy99MouO2rz7I  o23g4M2rZII7GuRnGCId  =o+XM  -----END PGP SIGNATURE-----
    Added encryption support via openssl smime

--- dump-database.sh
+++ dump-database.sh
@@ -24,28 +24,27 @@ CONFIG_FILE="/etc/dump-database.conf"
 
 # Output to stdout and to file
 output() {
-	MSG="${1}"		# Message to output
-	LOG="${2}"		# Log? 1: yes 0: No
-	FILE="${3}"		# Logfile path
+	local MSG="${1}"		# Message to output
+	local LOG="${2:-0}"	# Log? 1: yes 0: No (defaults to 0)
+	local FILE="${3}"		# Logfile path
 
 	printf "%s\n" "${MSG}"
 	[ ${LOG} -eq 1 ] && printf "%s %s %s\n" "$(date '+%Y-%m-%d')" "$(date '+%H:%M:%S')" "${MSG}" >> "${FILE}"
 }
-
 # Inline Output to stdout and to file (no newline)
 outputi() {
-	MSG="${1}"		# Message to output
-	LOG="${2}"		# Log? 1: yes 0: No
-	FILE="${3}"		# Logfile path
+	local MSG="${1}"		# Message to output
+	local LOG="${2:-0}"	# Log? 1: yes 0: No (defaults to 0)
+	local FILE="${3}"		# Logfile path
 
 	printf "%s" "${MSG}"
 	[ ${LOG} -eq 1 ] && printf "%s %s %s" "$(date '+%Y-%m-%d')" "$(date '+%H:%M:%S')" "${MSG}" >> "${FILE}"
 }
 # Output to stdout and to file (no time)
 outputn() {
-	MSG="${1}"		# Message to output
-	LOG="${2}"		# Log? 1: yes 0: No
-	FILE="${3}"		# Logfile path
+	local MSG="${1}"		# Message to output
+	local LOG="${2:-0}"	# Log? 1: yes 0: No (defaults to 0)
+	local FILE="${3}"		# Logfile path
 
 	printf "%s\n" "${MSG}"
 	[ ${LOG} -eq 1 ] && printf "%s\n" "${MSG}" >> "${FILE}"
@@ -55,8 +54,8 @@ outputn() {
 
 # 1.) ---- Read configuration file
 if [ ! -f "${CONFIG_FILE}" ]; then
-	echo "Configuration file not found in ${CONFIG_FILE}"
-	echo "Aborting"
+	output "[ERR]  Configuration file not found in ${CONFIG_FILE}"
+	output "Aborting"
 	exit 1
 fi
 . "${CONFIG_FILE}"
@@ -65,34 +64,34 @@ fi
 
 # 2.) ---- Read Logging options
 if [ -z ${LOG+x} ]; then
-	echo 'Missing $LOG variable in ${CONFIG_FILE}'
-	echo "Aborting"
-	exit 1
+	output '[WARN] Missing $LOG variable in ${CONFIG_FILE}'
+	output "[WARN] Logging disabled"
+	LOG=0
 fi
 if [ ${LOG} -eq 1 ]; then
 	if [ -z ${LOGFILE+x} ]; then
-		echo 'Missing $LOGFILE variable in ${CONFIG_FILE}'
-		echo "Logging disabled"
+		output '[WARN] Missing $LOGFILE variable in ${CONFIG_FILE}'
+		output "[WARN] Logging disabled"
 		LOG=0
 	else
 		if [ ! -f "${LOGFILE}" ]; then
-			echo "Logfile does not exist in ${LOGFILE}"
-			echo "Trying to create..."
+			output "[WARN] Logfile does not exist in ${LOGFILE}"
+			output "[INFO] Trying to create..."
 			touch "${LOGFILE}" > /dev/null 2>&1
 
 			if [ $? -ne 0 ]; then
-				echo "Failed to create file ${LOGFILE}"
-				echo "Logging disabled"
+				output "[ERR]  Failed to create file ${LOGFILE}"
+				output "[WARN] Logging disabled"
 				LOG=0
 			else
-				echo "Created file ${LOGFILE}"
-				echo "Logging enabled"
+				output "[INFO] Created file ${LOGFILE}"
+				output "[INFO] Logging enabled"
 				LOG=1
 			fi
 		else
 			if [ ! -w "${LOGFILE}" ]; then
-				echo "Logfile ${LOGFILE} not writeable"
-				echo "Logging disabled"
+				output "[WARN] Logfile ${LOGFILE} not writeable"
+				output "[WARN] Logging disabled"
 				LOG=0
 			fi
 		fi
@@ -108,8 +107,8 @@ fi
 # 3.) ---- Destination dir
 # Check if destination dir exists
 if [ ! -d "${TARGET}" ]; then
-	output "Destination dir ${TARGET} does not exist" $LOG "${LOGFILE}"
-	outputi "Trying to create... " $LOG "${LOGFILE}"
+	output "[WARN] Destination dir ${TARGET} does not exist" $LOG "${LOGFILE}"
+	outputi "[INFO] Trying to create... " $LOG "${LOGFILE}"
 	mkdir -p "${TARGET}" > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		outputn "Failed" $LOG "${LOGFILE}"
@@ -117,14 +116,14 @@ if [ ! -d "${TARGET}" ]; then
 		exit 1
 	else
 		output "Done"
-		output "Adjusting file permissions on ${TARGET}" $LOG "${LOGFILE}"
+		output "[INFO] Adjusting file permissions on ${TARGET}" $LOG "${LOGFILE}"
 		chmod 0700 "${TARGET}"
 	fi
 fi
 # Check if destination dir is writeable
 if [ ! -w "${TARGET}" ]; then
-	output "Destination dir ${TARGET} is not writeable" $LOG "${LOGFILE}"
-	outputi "Trying to chmod... " $LOG "${LOGFILE}"
+	output "[WARN] Destination dir ${TARGET} is not writeable" $LOG "${LOGFILE}"
+	outputi "[INFO] Trying to chmod... " $LOG "${LOGFILE}"
 	chmod 0700 "${TARGET}" > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		outputn "Failed" $LOG "${LOGFILE}"
@@ -133,7 +132,7 @@ if [ ! -w "${TARGET}" ]; then
 	else
 		output "Done" $LOG "${LOGFILE}"
 	fi
-	outputi "Trying to chown... " $LOG "${LOGFILE}"
+	outputi "[INFO] Trying to chown... " $LOG "${LOGFILE}"
 	chown "$(whoami)" "${TARGET}" > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		outputn "Failed" $LOG "${LOGFILE}"
@@ -147,20 +146,32 @@ fi
 
 # 4.) ---- Check binaries
 if ! command -v mysql > /dev/null 2>&1 ; then
-	output "'mysql' not found" $LOG "${LOGFILE}"
+	output "[ERR]  'mysql' not found" $LOG "${LOGFILE}"
 	output "Aborting" $LOG "${LOGFILE}"
 	exit 2
 fi
 if ! command -v mysqldump > /dev/null 2>&1 ; then
-	output "'mysqldump' not found" $LOG "${LOGFILE}"
+	output "[ERR]  'mysqldump' not found" $LOG "${LOGFILE}"
 	output "Aborting" $LOG "${LOGFILE}"
 	exit 2
 fi
 if [ ${COMPRESS} -eq 1 ]; then
 	if ! command -v gzip > /dev/null 2>&1 ; then
-		output "'gzip' not found" $LOG "${LOGFILE}"
-		output "Disabling compression" $LOG "${LOGFILE}"
+		output "[WARN] 'gzip' not found" $LOG "${LOGFILE}"
+		output "[WARN] Disabling compression" $LOG "${LOGFILE}"
 		COMPRESS=0
+	fi
+fi
+if [ ${ENCRYPT} -eq 1 ]; then
+	if ! command -v openssl > /dev/null 2>&1 ; then
+		output "[WARN] 'openssl' not found" $LOG "${LOGFILE}"
+		output "[WARN] Disabling encryption" $LOG "${LOGFILE}"
+		ENCRYPT=0
+	fi
+	if [ ! -f "${OPENSSL_PUBKEY_PEM}" ]; then
+		output "[WARN] OpenSSL pubkey not found in ${OPENSSL_PUBKEY_PEM}"
+		output "[WARN] Disabling encryption" $LOG "${LOGFILE}"
+		ENCRYPT=0
 	fi
 fi
 
@@ -170,6 +181,7 @@ fi
 MYSQL="$(which mysql)"
 MYSQLDUMP="$(which mysqldump)"
 GZIP="$(which gzip)"
+OPENSSL="$(which openssl)"
 
 # Date/Time Prefix
 DATE="$(date '+%Y-%m-%d')"
@@ -205,11 +217,21 @@ for db in ${DATABASES}; do
 	if [ ${skip} -eq 0 ]; then
 		starttime=$(date +%s)
 		if [ ${COMPRESS} -eq 1 ]; then
-			outputi "Dumping:  ${db} (compressed) " $LOG "${LOGFILE}"
-			${MYSQLDUMP} ${MYSQL_OPTS} --user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST} "${db}" | ${GZIP} -9 > "${TARGET}/${DATE}_${TIME}__${db}.sql.gz"
+			if [ ${ENCRYPT} -eq 1 ]; then
+				outputi "Dumping:  ${db} (compressed) (encrypted) " $LOG "${LOGFILE}"
+				${MYSQLDUMP} ${MYSQL_OPTS} --user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST} "${db}" | ${GZIP} -9 | ${OPENSSL} smime -encrypt -binary -text ${OPENSSL_ALGO_ARG} -out "${TARGET}/${DATE}_${TIME}__${db}.sql.gz.enc" -outform DER ${OPENSSL_PUBKEY_PEM}
+			else
+				outputi "Dumping:  ${db} (compressed) " $LOG "${LOGFILE}"
+				${MYSQLDUMP} ${MYSQL_OPTS} --user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST} "${db}" | ${GZIP} -9 > "${TARGET}/${DATE}_${TIME}__${db}.sql.gz"
+			fi
 		else
-			outputi "Dumping:  ${db} " $LOG "${LOGFILE}"
-			${MYSQLDUMP} ${MYSQL_OPTS} --user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST} "${db}" > "${TARGET}/${DATE}_${TIME}__${db}.sql"
+			if [ ${ENCRYPT} -eq 1 ]; then
+				outputi "Dumping:  ${db} (encryped) " $LOG "${LOGFILE}"
+				${MYSQLDUMP} ${MYSQL_OPTS} --user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST} "${db}" | ${OPENSSL} smime -encrypt -binary -text ${OPENSSL_ALGO_ARG} -out "${TARGET}/${DATE}_${TIME}__${db}.sql.enc" -outform DER ${OPENSSL_PUBKEY_PEM}
+			else
+				outputi "Dumping:  ${db} " $LOG "${LOGFILE}"
+				${MYSQLDUMP} ${MYSQL_OPTS} --user=${MYSQL_USER} --password=${MYSQL_PASS} --host=${MYSQL_HOST} "${db}" > "${TARGET}/${DATE}_${TIME}__${db}.sql"
+			fi
 		fi
 		# We cannot check against $? as the first if uses a pipe | gzip and so we
 		# need to check against the exit code of the first pipe status
