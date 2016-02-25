63e408d828e9015c3a30b7230f97688170eee729
cytopia <cytopia@everythingcli.org>
2016-02-25 18:36:06 +0100

    Checking MySQL Dump options

--- .travis/01-check_normal-operation.sh
+++ .travis/01-check_normal-operation.sh
@@ -3,6 +3,15 @@
 ERROR=0
 
 
+txtgrn=$(tput setaf 2) # Green
+txtylw=$(tput setaf 3) # Yellow
+txtblu=$(tput setaf 4) # Blue
+txtpur=$(tput setaf 5) # Purple
+txtcyn=$(tput setaf 6) # Cyan
+txtwht=$(tput setaf 7) # White
+txtrst=$(tput sgr0) # Text reset.
+
+
 echo "##########################################################################################"
 echo "#"
 echo "#  1.  C H E C K I N G   N O R M A L   O P E R A T I O N"
@@ -23,14 +32,14 @@ echo "----------------------------------------"
 echo " 1.1.1 Test mode first run"
 echo "----------------------------------------"
 echo "\$ mysqldump-secure --test --verbose"
-sudo mysqldump-secure --test --verbose && echo "--> [OK] Expected" || { echo "--> [FAIL] Unexpected exit code: $?"; ERROR=1; }
+sudo mysqldump-secure --test --verbose && echo "${txtgrn}===> [OK] Success${txtrst}" || { echo "${txtpur}===> [FAIL] Unexpected exit code: $?${txtrst}"; ERROR=1; }
 
 echo
 echo "----------------------------------------"
 echo " 1.1.2 Test mode second run"
 echo "----------------------------------------"
 echo "\$ mysqldump-secure --test --verbose"
-sudo mysqldump-secure --test --verbose && echo "--> [OK] Expected" || { echo "--> [FAIL] Unexpected exit code: $?"; ERROR=1; }
+sudo mysqldump-secure --test --verbose && echo "${txtgrn}===> [OK] Success${txtrst}" || { echo "${txtpur}===> [FAIL] Unexpected exit code: $?${txtrst}"; ERROR=1; }
 
 
 
@@ -50,7 +59,7 @@ sudo rm /var/log/mysqldump-secure.log 2>/dev/null
 sudo rm /var/log/mysqldump-secure.nagios.log 2>/dev/null
 sudo rm -rf /var/mysqldump-secure/ 2>/dev/null
 echo "\$ mysqldump-secure --verbose"
-sudo mysqldump-secure --verbose && echo "--> [OK] Expected" || { echo "--> [FAIL] Unexpected exit code: $?"; ERROR=1; }
+sudo mysqldump-secure --verbose && echo "${txtgrn}===> [OK] Success${txtrst}" || { echo "${txtpur}===> [FAIL] Unexpected exit code: $?${txtrst}"; ERROR=1; }
 
 echo
 echo "----------------------------------------"
@@ -58,7 +67,7 @@ echo " 1.2.2 Normal mode second run"
 echo "----------------------------------------"
 sudo rm -rf /var/mysqldump-secure/ && sudo mkdir -p /var/mysqldump-secure/ && sudo chmod 0700 /var/mysqldump-secure/
 echo "\$ mysqldump-secure --verbose"
-sudo mysqldump-secure --verbose && echo "--> [OK] Expected" || { echo "--> [FAIL] Unexpected exit code: $?"; ERROR=1; }
+sudo mysqldump-secure --verbose && echo "${txtgrn}===> [OK] Success${txtrst}" || { echo "${txtpur}===> [FAIL] Unexpected exit code: $?${txtrst}"; ERROR=1; }
 
 echo
 echo "----------------------------------------"
@@ -70,7 +79,7 @@ sudo touch -a -m -t 201512180130.09 /var/mysqldump-secure/delete-me-2.txt
 sudo touch -a -m -t 201512180130.09 /var/mysqldump-secure/delete-me-3.txt
 sudo touch -a -m -t 201512180130.09 /var/mysqldump-secure/delete-me-4.txt
 echo "\$ mysqldump-secure --verbose"
-sudo mysqldump-secure --verbose && echo "--> [OK] Expected" || { echo "--> [FAIL] Unexpected exit code: $?"; ERROR=1; }
+sudo mysqldump-secure --verbose && echo "${txtgrn}===> [OK] Success${txtrst}" || { echo "${txtpur}===> [FAIL] Unexpected exit code: $?${txtrst}"; ERROR=1; }
 
 
 
@@ -91,7 +100,7 @@ sudo rm /var/log/mysqldump-secure.log 2>/dev/null
 sudo rm /var/log/mysqldump-secure.nagios.log 2>/dev/null
 sudo rm -rf /var/mysqldump-secure/ 2>/dev/null
 echo "\$ mysqldump-secure --cron"
-sudo mysqldump-secure --cron && echo "--> [OK] Expected" || { echo "--> [FAIL] Unexpected exit code: $?"; ERROR=1; }
+sudo mysqldump-secure --cron && echo "${txtgrn}===> [OK] Success${txtrst}" || { echo "${txtpur}===> [FAIL] Unexpected exit code: $?${txtrst}"; ERROR=1; }
 
 echo
 echo "----------------------------------------"
@@ -99,7 +108,7 @@ echo " 1.3.2 Cron mode second run"
 echo "----------------------------------------"
 sudo rm -rf /var/mysqldump-secure/ && sudo mkdir -p /var/mysqldump-secure/ && sudo chmod 0700 /var/mysqldump-secure/
 echo "\$ mysqldump-secure --cron"
-sudo mysqldump-secure --cron && echo "--> [OK] Expected" || { echo "--> [FAIL] Unexpected exit code: $?"; ERROR=1; }
+sudo mysqldump-secure --cron && echo "${txtgrn}===> [OK] Success${txtrst}" || { echo "${txtpur}===> [FAIL] Unexpected exit code: $?${txtrst}"; ERROR=1; }
 
 echo
 echo "----------------------------------------"
@@ -111,7 +120,7 @@ sudo touch -a -m -t 201512180130.09 /var/mysqldump-secure/delete-me-2.txt
 sudo touch -a -m -t 201512180130.09 /var/mysqldump-secure/delete-me-3.txt
 sudo touch -a -m -t 201512180130.09 /var/mysqldump-secure/delete-me-4.txt
 echo "\$ mysqldump-secure --cron"
-sudo mysqldump-secure --cron && echo "--> [OK] Expected" || { echo "--> [FAIL] Unexpected exit code: $?"; ERROR=1; }
+sudo mysqldump-secure --cron && echo "${txtgrn}===> [OK] Success${txtrst}" || { echo "${txtpur}===> [FAIL] Unexpected exit code: $?${txtrst}"; ERROR=1; }
 
 
 
@@ -128,28 +137,28 @@ echo "----------------------------------------"
 echo " 1.4.1 --help"
 echo "----------------------------------------"
 echo "\$ mysqldump-secure --help"
-sudo mysqldump-secure --help && echo "--> [OK] Expected" || { echo "--> [FAIL] Unexpected exit code: $?"; ERROR=1; }
+sudo mysqldump-secure --help && echo "${txtgrn}===> [OK] Success${txtrst}" || { echo "${txtpur}===> [FAIL] Unexpected exit code: $?${txtrst}"; ERROR=1; }
 
 echo
 echo "----------------------------------------"
 echo " 1.4.2 --conf (does not exist)"
 echo "----------------------------------------"
 echo "\$ mysqldump-secure --verbose --conf=/etc/nothere"
-sudo mysqldump-secure --verbose --conf=/etc/nothere && { echo "--> [FAIL] Unexpected OK. Exit code: $?"; ERROR=1; } || echo "--> [OK] Expected Error. Exit code: $?"
+sudo mysqldump-secure --verbose --conf=/etc/nothere && { echo "${txtpur}===> [FAIL] Unexpected OK${txtrst}"; ERROR=1; } || echo "${txtgrn}===> [OK] Expected Error. Exit code: $?${txtrst}"
 
 echo
 echo "----------------------------------------"
 echo " 1.4.3 --conf (random file)"
 echo "----------------------------------------"
 echo "\$ mysqldump-secure --verbose --conf=/etc/mysqldump-secure.cnf"
-sudo mysqldump-secure --verbose --conf=/etc/mysqldump-secure.cnf && { echo "--> [FAIL] Unexpected OK. Exit code: $?"; ERROR=1; } || echo "--> [OK] Expected Error. Exit code: $?"
+sudo mysqldump-secure --verbose --conf=/etc/mysqldump-secure.cnf && { echo "${txtpur}===> [FAIL] Unexpected OK${txtrst}"; ERROR=1; } || echo "${txtgrn}===> [OK] Expected Error. Exit code: $?${txtrst}"
 
 echo
 echo "----------------------------------------"
 echo " 1.4.4 wrong argument"
 echo "----------------------------------------"
 echo "\$ mysqldump-secure --wrong"
-sudo mysqldump-secure --wrong && { echo "--> [FAIL] Unexpected OK. Exit code: $?"; ERROR=1; } || echo "--> [OK] Expected Error. Exit code: $?"
+sudo mysqldump-secure --wrong && { echo "${txtpur}===> [FAIL] Unexpected OK${txtrst}"; ERROR=1; } || echo "${txtgrn}===> [OK] Expected Error. Exit code: $?${txtrst}"
 
 
 
